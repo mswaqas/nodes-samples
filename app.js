d667fbe908fe576d220319f2e2289dabd037721b
Waqas Shaikh <waqas@shopiniworld.com>
2021-01-16 03:11:45 +0400

    chalk package for console error styling

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const additions = require('./notes');
+const chalk = require('chalk');
+
+const success = chalk.bold.green;
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
 
 let sum = additions(10, -2);
 
-console.log(sum);
+console.log(chalk.rgb(123, 45, 67).underline('Total Sum of calculation is ') + success(sum));
