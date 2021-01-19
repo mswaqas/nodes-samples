f6263ff9d24ced0a178a416c706c00894558232b
Waqas Shaikh <waqas@shopiniworld.com>
2021-01-19 22:18:18 +0400

    overwrite json file data

--- app.js
+++ app.js
@@ -1,54 +1,15 @@
-const yargs = require('yargs');
-const chalk = require('chalk');
-const additions = require('./notes');
+const fs = require('fs')
+
+const dataBuffer = fs.readFileSync('json-data/1-json-data.json')
+const dataString = dataBuffer.toString()
+const data = JSON.parse(dataString)
+
+console.log(data);
+
+data.name = 'Ramzan Khatri'
+data.age = 24
 
 
-yargs.version('1.1.0');
+const userData = JSON.stringify(data);
 
-yargs.command({
-    'command':'add',
-    'description':'Adding notes command',
-    builder:{
-        body:{
-            descript:'Note Body',
-            demandOption:'required',
-            type:'string'
-        },
-        title:{
-            descript:'Note Title',
-            demandOption:'required',
-            type:'string'
-        }
-    },
-    handler:function(argv){
-        console.log(chalk.green.bold('Title : '+argv.title))
-        console.log(chalk.red.bold('Body : '+argv.body))
-
-    }
-});
-
-yargs.command({
-    'command':'remove',
-    'description':'remove notes command',
-    handler:function(){
-        console.log('Remove notes command running')
-    }
-});
-
-yargs.command({
-    'command':'list',
-    'description':'List notes command',
-    handler:function(){
-        console.log('Listing notes command running')
-    }
-});
-
-yargs.command({
-    'command':'read',
-    'description':'Read notes command',
-    handler:function(){
-        console.log('Read notes command running')
-    }
-});
-
-yargs.parse();
+fs.writeFileSync('json-data/1-json-data.json',userData);
