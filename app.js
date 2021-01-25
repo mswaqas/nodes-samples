ad009bda416adce993d13dda3312bbd426e45198
Waqas Shaikh <waqas@shopiniworld.com>
2021-01-25 21:46:36 +0400

    notes application

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
+
+const yargs = require('yargs');	
 const fs = require('fs')
+const chalk = require('chalk');	
+const notes = require('./notes');	
 
-const dataBuffer = fs.readFileSync('json-data/1-json-data.json')
-const dataString = dataBuffer.toString()
-const data = JSON.parse(dataString)
-
-console.log(data);
-
-data.name = 'Ramzan Khatri'
-data.age = 24
+yargs.version('1.1.0');
 
 
-const userData = JSON.stringify(data);
+yargs.command({
+    'command':'add',
+    'description':'Adding notes command',	
+    builder:{	
+        body:{	
+            descript:'Note Body',	
+            demandOption:'required',	
+            type:'string'	
+        },	
+        title:{	
+            descript:'Note Title',	
+            demandOption:'required',	
+            type:'string'	
+        }	
+    },	
+    handler:function(argv){	
+        notes.addNotes(argv.title,argv.body);
+    }	
+});	
 
-fs.writeFileSync('json-data/1-json-data.json',userData);
+yargs.command({
+    'command':'remove',
+    'description':'remove notes command',	
+    builder:{		
+        title:{	
+            descript:'Note Title',	
+            demandOption:'required',	
+            type:'string'	
+        }	
+    },	
+    handler:function(argv){	
+        notes.deleteNotes(argv.title);
+    }	
+});	
+
+yargs.parse(); 
--- notes.js
+++ notes.js
@@ -1,5 +1,49 @@
-const add = function (a, b) {
-    return a + b;
+
+const fs = require('fs')
+const chalk = require('chalk');	
+
+const addNotes = function (title, body) {
+    const notes = loadNotes()
+    
+    notes.push({
+        'title':title,
+        'body':body
+    })
+
+    saveNotes(notes);
 }
 
-module.exports = add;
+const deleteNotes = function(title){
+    const noteslist = loadNotes()
+    const notesToKeep = noteslist.filter(function(note){
+        return note.title !== title
+    })
+
+    if(noteslist.length > notesToKeep.length){
+        console.log(chalk.green('note removed'));
+    }else{
+        console.log(chalk.red('note is not exist'));
+    }
+    saveNotes(notesToKeep)
+}
+
+const saveNotes = function(notes){
+    const  JsonData = JSON.stringify(notes);
+    fs.writeFileSync('notes.json',JsonData); 
+}
+
+const loadNotes = function(){
+
+    try{
+        const fileBuffer = fs.readFileSync('notes.json')
+        const fileData = fileBuffer.toString()
+        return JSON.parse(fileData) 
+    }
+    catch(e){
+        return [];
+    }
+}
+module.exports = {
+    'addNotes': addNotes,
+    'deleteNotes':deleteNotes
+}
